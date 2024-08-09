ca813b8b863e95b2a3f66d9557e6b3e34979c611
BigRedSped-Laptop <98977703+Nathancecixx@users.noreply.github.com>
2024-08-09 14:03:42 -0400

    Added responsiveness to new sections
    Added responsiveness for new About Me and Expertise sections

--- src/Data/Skills.js
+++ src/Data/Skills.js
@@ -11,7 +11,7 @@ export const skills = [
             "One of the best skills I have in my arsenal is my familiarity with the Object Oriented approach to programming. Right from the jump I learned to program with these concepts as in grade 10 my first real language was java. Over the years I have increased my familiarity with not only java, but the OOP concept itself with other languages such as C++, and C#",
         image: OOP_Logo,
         link: "https://reactbootcamp.com",
-        techStack: ['C', 'C++', "Object Oriented Programming"]
+        techStack: ['C++', 'Java']
     },
     {
         id: "2",
@@ -21,7 +21,7 @@ export const skills = [
             "I was introduced to data structures and algorithms during my first year of college. Since then I have researched and implemented plenty of different algorithms from basic sorting, to Huffman's Encoding for file compression.",
         image: DSA,
         link: "https://reedbarger.com",
-        techStack: ['C', 'C++', "Object Oriented Programming"]
+        techStack: ['C', 'C++', "Python"]
     },
     {
         id: "3",
@@ -31,6 +31,6 @@ export const skills = [
             "During the time ive been in school I have had the opportunity to experience plenty of group work through projects. I worked with range of people on a wide variety of problems in C ranging from triangle and different shape classifiers, basic file encryption software, or even an implementation of the machine learning K Nearest Neighbors algorithm (K-NN). Each of these ventures where vastly successful being great opportunities to learn.",
         image: Handshake,
         link: "https://jsbootcamp.com",
-        techStack: ['C', 'C++', "Object Oriented Programming"]
+        techStack: ['C', 'C++', "Machine Learning"]
     },
 ];
--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -3,13 +3,13 @@ import React from 'react'
 import "./AboutMe.css"
 import InfoCard from "../Components/InfoCard/InfoCard";
 import C_Logo from "../Resources/Icons/C_Logo.png";
-import CodingStudent from "../Resources/Icons/CodingStudent.png";
+import CodingStudent from "../Resources/Icons/AboutMeIcon.png";
 const AboutMe = () => {
     return (
         <div className="about-me-section">
             <div className="paragraph-section">
-                <img className="paragraph-image" src={CodingStudent} alt={"Coding Student Logo"}/>
                 <div className="paragraph-text">
+                    <img className="paragraph-image" src={CodingStudent} alt={"Coding Student Logo"}/>
                     <h1 className="h1">About Me</h1>
                     <p className="p1">My journey into computer science began in 2020 with an introductory programming class in high school, where I quickly discovered my passion for technology. Despite challenges in other subjects, my dedication led me to complete an additional year of high school to meet university requirements. This determination paid off, and I am now in my second year of the Bachelor of Computer Science program at Conestoga College. I'm continually exploring the ever-evolving world of technology, driven by a deep curiosity and commitment to learning.</p>
                 </div>
